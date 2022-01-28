c6505bde55171841da51bca6fe340d873c1c651c
Bondhan Mondal <bondhan2725@gmail.com>
2022-01-28 20:59:04 +0600

    conditional added

---
+++ conditional.js
@@ -0,0 +1,32 @@
+var isFoodReady = true;
+
+// if(isFoodReady == true){
+//     console.log('vat khamu');
+// }
+
+var iphonePrice = 70000;
+var myBudget = 300000;
+
+// if (iphonePrice < myBudget) {
+//     console.log('iphone diye futani marmu');
+// }
+// if (iphonePrice > myBudget) {
+//     console.log('xiaomi is the best');
+// }
+
+// if (iphonePrice < myBudget) {
+//     console.log('iphone diye futani marmu');
+// }
+// else{
+//     console.log('xiaomi is the best');
+// }
+
+var chickenPrice = 180;
+var myMoney = 850;
+
+if (chickenPrice < myMoney) {
+    console.log('yes!! murgir ran khamu r haddi chibamu'); 
+}
+else {
+    console.log('smashed potato with lentil soup');
+}
