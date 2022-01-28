9a61e43616164166e09497e8e6c785ec8888007b
Bondhan Mondal <bondhan2725@gmail.com>
2022-01-28 21:11:11 +0600

    multiple condition added

---
+++ multiple-condition.js
@@ -0,0 +1,35 @@
+var gotJob = true;
+var moneySaved = 250000;
+var hasFlat = true;
+var hasHouse = false;
+
+// if (gotJob == true && moneySaved >200000) {
+//     console.log('cholo biye kore feli');
+// }
+// else {
+//     console.log('Tor kopale biye nai');
+// }
+
+
+// if (gotJob == true && moneySaved >200000 && hasFlat == true) {
+//     console.log('cholo biye kore feli');
+// }
+// else {
+//     console.log('Tor kopale biye nai');
+// }
+
+
+// if (gotJob == true || moneySaved >200000 || hasFlat == true) {
+//     console.log('cholo biye kore feli');
+// }
+// else {
+//     console.log('Tor kopale biye nai');
+// }
+
+
+if ((gotJob == true && moneySaved >200000 ) || hasHouse == true) {
+    console.log('cholo biye kore feli');
+}
+else {
+    console.log('Tor kopale biye nai');
+}
