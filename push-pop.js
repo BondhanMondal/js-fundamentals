06a382fb605587c8130a49038f17b4d8bd46fd3f
Bondhan Mondal <bondhan2725@gmail.com>
2022-01-28 19:45:04 +0600

    push-pop added

---
+++ push-pop.js
@@ -0,0 +1,18 @@
+// add elements to an array 
+var lastBench = ['kalam', 'balam', 'salam'];
+lastBench.push('jalam');
+lastBench.push('palam');
+lastBench.push('malam');
+// console.log(lastBonch);
+
+var friendsAge = [11, 13, 17, 12];
+// friendsAge.push(15);
+// console.log(friendsAge);
+
+// remove elements from an array 
+var lastitem = friendsAge.pop();
+// friendsAge.pop();
+console.log(friendsAge);
+console.log(lastitem);
+
+
