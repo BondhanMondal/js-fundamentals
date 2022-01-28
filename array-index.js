e7195fce185be73f0214cfcf061d5dd4535d427c
Bondhan Mondal <bondhan2725@gmail.com>
2022-01-28 16:07:38 +0600

    array index added

---
+++ array-index.js
@@ -0,0 +1,15 @@
+var bookPages =['hable', 'blood', 'chouddogusti', 'battery', 'paracetamol', 'gymnasium'];
+
+var batteryIndex = bookPages.indexOf('battery');
+console.log(batteryIndex);
+
+var numbers =[45, 98, 65, 72, 52, 63];
+var index = numbers.indexOf(72);
+console.log(index);
+
+var secondIndex = bookPages[2];
+console.log(secondIndex);
+
+numbers[1] =95;
+numbers[0] =5;
+console.log(numbers);
