cccb59535f65007247fb000f2dac4cf8964b9ac0
Bondhan Mondal <bondhan2725@gmail.com>
2022-01-28 15:49:14 +0600

    array added

---
+++ array.js
@@ -0,0 +1,14 @@
+var friendsAge = [11, 13, 17];
+
+var muriChanachur_Fee = [35, 55, 221, 3];
+
+var nayikas = ["mahi", "opu", "sabana", "kopila"];
+
+var oddNumbers = [1, 3, 5, 7, 9];
+
+var vowels =['a', 'e', 'i', 'o', 'u'];
+
+// console.log(vowels.length);
+
+var vowelsCount = vowels.length;
+console.log(vowelsCount);
