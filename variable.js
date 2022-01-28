6e9c369908848edd40e0fa4ce136df5b05de4789
Bondhan Mondal <bondhan2725@gmail.com>
2022-01-28 15:30:02 +0600

    variable recap

---
+++ variable.js
@@ -0,0 +1,16 @@
+// number 
+var watchPrice = 120;
+watchPrice = 125;
+watchPrice++;
+watchPrice--;
+console.log(watchPrice);
+
+// string 
+var userName = 'Sogir uddin';
+
+// booleam 
+var isExpensive = true;
+
+
+// Integer: 1 2 3 4 5 6 7 8 9 
+// Float: 2.714 5.321 3.14 
